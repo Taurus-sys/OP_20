6698440c1b330fa097a083b9079f9639f901be1c
Taurus-sys <jobcheh@gmail.com>
2025-07-16 13:26:45 +0300

    Update MyToken.ts

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -21,10 +21,10 @@ export class MyToken extends DeployableOP_20 {
 
     // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
     public override onDeployment(_calldata: Calldata): void {
-        const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
-        const decimals: u8 = 18; // Your decimals.
-        const name: string = 'Test'; // Your token name.
-        const symbol: string = 'TEST'; // Your token symbol.
+        const maxSupply: u256 = u256.fromString('100000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
+        const decimals: u8 = 16; // Your decimals.
+        const name: string = 'Test1'; // Your token name.
+        const symbol: string = 'TEST1'; // Your token symbol.
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
@@ -105,3 +105,4 @@ export class MyToken extends DeployableOP_20 {
         return writer;
     }
 }
+    
